7ee5c6eaa725a4e86578f2e33f07c63dd49c5171
Blashyrkh2124 <w.blokker@gmail.com>
2018-12-30 02:24:39 +0100

    Solve excersie 2.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text, timesRepeat) {
+  var textRepeated = text;
+  if (timesRepeat > 0) {
+  for (i = 1; i < timesRepeat; i++) {
+    textRepeated = textRepeated.concat(text);
+  } return textRepeated
+} else if (timesRepeat == 0) {
+  return "";
+} else {return "ERROR"};
+};
 
-}
-
+repeatString("test", 3);
 module.exports = repeatString
