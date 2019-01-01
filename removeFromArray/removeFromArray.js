81e8c6e3622a78291f7b3bac9bc508afb3452ac4
Blashyrkh2124 <w.blokker@gmail.com>
2019-01-02 00:56:55 +0100

    Solve removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,7 +1,16 @@
-const removeFromArray = function(arr, haveRemoved()) {
-  console.log(haveRemoved);
-}
-
-console.log(removeFromArray([1, 2, 3, 4], 3, 2))
+const removeFromArray = function(arr, ...others) {
+  let val = others;
+  var newArray = arr;
+  var i = 0;
+  do {
+    if ((arr.indexOf(val[i])) != -1) {
+      var pos = arr.indexOf(val[i]);
+      arr.splice(pos, 1);
+      newArray = arr;
+      i++;
+    } else {i++}
+  } while (i < val.length);
+  return newArray;
+};
 
 module.exports = removeFromArray
