d616f986044122e05fa6234d9e3054f6103bb16e
Blashyrkh2124 <w.blokker@gmail.com>
2019-01-05 04:29:34 +0100

    Solve sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,6 +1,25 @@
 const sumAll = function(a, b) {
-  a.reduce
-    return a + b; }, 0);
+  var lowNum = 1;
+  var highNum = 2;
+
+  if (typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  } else if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (a > b) {
+  lowNum = b;
+  highNum = a;
+  } else {
+  lowNum = a;
+  highNum = b;
+};
+  var i = lowNum;
+  var numArray = [];
+  for (i; i <= highNum; i++) {
+    numArray.push(i);
+  }
+  var sum = numArray.reduce(function(a, b) { return a + b; }, 0);
+  return sum;
 }
 
 module.exports = sumAll
